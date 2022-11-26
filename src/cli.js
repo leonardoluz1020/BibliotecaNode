09d4dd7ef5f8f2e50fb7609bab780fe375c23310
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-26 19:45:26 -0300

    Criando lista validada

--- src/cli.js
+++ src/cli.js
@@ -1,38 +1,48 @@
-import chalk from 'chalk';
-import fs from 'fs';
-import pegarArquivo from './index.js';
-
+import chalk from 'chalk'
+import fs from 'fs'
+import pegarArquivo from './index.js'
+import listaValidada from './http.js'
 const caminho = process.argv
 
-function imprimirTexto(lista, arquivo = '') {
-    console.log(
-        chalk.yellow('Lista de links'),
-        chalk.black.bgGreen(arquivo),
-        lista
-    )
+function imprimiLista(valida, lista, arquvivo = '') {
+    
+    if (valida) {
+        console.log(
+            chalk.yellow('Lista validada'),
+            chalk.black.bgBlue(arquvivo),
+            listaValidada(lista)
+        )
+    }else{
+        console.log(
+            chalk.yellow('Lista de links'),
+            chalk.black.bgBlue(arquvivo),
+            lista
+        )
+    }
 }
 
 async function processarTexto(argumento) {
     const caminho = argumento[2]
+    const valida = argumento[3] === '--valida';
+
 
     try {
         fs.lstatSync(caminho)
     } catch (error) {
         if (error.code === 'ENOENT') {
-            console.log(chalk.red('Caminho de pasta ou arquivo inválido'))
-            return
+            console.log(chalk.red('Caminho de pasta ou arquivo errado'))
         }
+        return
     }
-
     if (fs.lstatSync(caminho).isFile()) {
         const lista = await pegarArquivo(caminho)
-        imprimirTexto(lista)
+        imprimiLista( valida ,lista)
     } else if (fs.lstatSync(caminho).isDirectory()) {
         const arquivos = await fs.promises.readdir(caminho)
         arquivos.forEach(async (nomeArquivo) => {
             const listas = await pegarArquivo(`${caminho}/${nomeArquivo}`)
-            imprimirTexto(listas, nomeArquivo)
-        })
+            imprimiLista(valida ,listas, nomeArquivo);
+        });
     }
 }
 
