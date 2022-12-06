f04588c964e7833da378eb6f3fef5434f9d164ef
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-06 18:07:04 -0300

    Constancia biblioteca node.js

--- src/index2.js
+++ src/index2.js
@@ -2,29 +2,28 @@ import chalk from 'chalk'
 import fs from 'fs'
 
 function tratarErro(error){
-    throw new Error(chalk.red(error.code, 'Não há arquvido no diretório'))
+    throw new Error(chalk.red(error.code, 'Não há arquivo no diretório!'))
 }
-
 
 function extrairLinks(texto){
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
     const capturas = [...texto.matchAll(regex)]
-    const resultados = capturas.map((captura) => ({[captura[1]]:captura[2]}))
+    const resultados = capturas.map((captura) => ({[captura[1]]: captura[2]}))
     return resultados.length !== 0 ? resultados : 'Não há link'
 }
 
-
 async function pegarArquivo(caminho) {
+    const encoding = 'utf-8'
     try {
-        // trabalhando com biblioteca markdown
-        const encoding = 'utf-8'
         const texto = await fs.promises.readFile(caminho, encoding)
         return extrairLinks(texto)        
     } catch (error) {
         tratarErro(error)
     }
+
 }
 
 export default pegarArquivo;
 
+// expressão regular
 //  /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
