6ed85d80f7d3b0851c8f92be5f94ba57bef9c39b
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-16 16:01:45 -0300

    Trabalhando com o index2 verif links

--- src/index2.js
+++ src/index2.js
@@ -1,27 +1,27 @@
-import chalk from 'chalk'
-import fs from 'fs'
-
-function extrairLinks(texto) {
-    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
-    const capturas = [...texto.matchAll(regex)]
-    const resultados = capturas.map((captura) => ({ [captura[1]] : captura[2] }))
-    return resultados.length !== 0 ? resultados : `Não há links`
-}
+import chalk from 'chalk';
+import fs from 'fs';
 
 function tratarErro(error){
-    throw new Error(chalk.red(error.code,'Não há arquivos no diretorio'))
+    throw new Error(chalk.red(error.code, 'Não há arquivo na pasta!.'))    
 }
 
-async function pegarArquivo(caminho) {
+function extrairLinks(texto){
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
+    const capturas = [...texto.matchAll(regex)]
+    const resultados = capturas.map((captura) => ({[captura[1]]: captura[2]}))
+    return resultados.length !== 0 ? resultados : 'Não há links no arquivo'
+}
+
+
+async function pegarArquivo(caminho){
     const encoding = 'utf-8'
     try {
-        const texto = await fs.promises.readFile(caminho, encoding)
-        return extrairLinks(texto)
+        const texto = await fs.promises.readFile(caminho,encoding)
+        return extrairLinks(texto)                
     } catch (error) {
-        tratarErro(error)
+        tratarErro(error)        
     }
 }
-
 
 export default pegarArquivo;
 
