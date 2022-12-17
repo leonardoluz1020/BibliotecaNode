713d8c235125bd483c476e18751721b7c2d80602
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-17 12:44:49 -0300

    Criando constancia em biblioteca node

--- src/cli-clone.js
+++ src/cli-clone.js
@@ -1,48 +1,48 @@
-import chalk from 'chalk'
-import fs from 'fs'
+import chalk from 'chalk';
+import fs from 'fs';
 import pegarArquivo from './index2.js'
 import listaValidada from './validacao-http.js'
 
 const caminho = process.argv
 
-async function imprimirLista(valida,lista, arquivo = ''){
-    if(valida){
+async function imprimirLista( valida,listas,nomeArquivo = ''){
+    if (valida) {
         console.log(
-            chalk.yellow('Lista de Links'),
-            chalk.bgBlue(arquivo),
-           await listaValidada(lista)
+            chalk.yellow('Lista de links'),
+            chalk.bgBlue(nomeArquivo),
+           await listaValidada(listas)
         )
     }else{
         console.log(
-            chalk.yellow('Lista de Links'),
-            chalk.bgBlue(arquivo),
-            lista
+            chalk.yellow('Lista de links'),
+            chalk.bgBlue(nomeArquivo),
+            listas
         )
     }
 }
 
 async function processarTexto(argumento){
-    const caminho = argumento[2]
-    const valida = argumento[3] === '--valida'
+    const caminho = argumento[2];
+    const valida = argumento[3] === '--valida';
     try {
         fs.lstatSync(caminho)
     } catch (error) {
-        if(error.code === 'ENOENT'){
-            console.log(chalk.red('Nome de pasta ou arquivo incorreto'))
+        if (error.code === 'ENOTFOUND') {
+            console.log('Caminho de arquivo ou pasta incorreto!.')
         }
-        return
-    }
-
+        return                
+    }   
     if(fs.lstatSync(caminho).isFile()){
-        const lista = await pegarArquivo(caminho)
-        imprimirLista(valida,lista)
+        const lista = await pegarArquivo(caminho);
+        imprimirLista( valida,lista)
     }else if(fs.lstatSync(caminho).isDirectory()){
-        const arquivos =await fs.promises.readdir(caminho)
-        arquivos.forEach(async(nomeArquivo)=> {
-            const listas =await pegarArquivo(`${caminho}/${nomeArquivo}`)
-            imprimirLista(valida,listas, nomeArquivo)
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async (nomeArquivo) => {
+            const listas = await pegarArquivo(`${caminho}/${nomeArquivo}`)
+            imprimirLista( valida,listas,nomeArquivo)
         })
-    }
+    }    
+    
 }
 
 processarTexto(caminho)
