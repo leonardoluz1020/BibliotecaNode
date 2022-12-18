c4baaeafa7c338a786df6c39923778a83e2a28e7
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-18 14:46:33 -0300

    Craindo validação de links http

--- src/validacao-http.js
+++ src/validacao-http.js
@@ -1,31 +1,28 @@
 function extrairLinks(lista) {
-    return lista.map((objLinks) => Object.values(objLinks).join())
+    return lista.map((link) => Object.values(link).join())
 }
-function remanejarErro(error) {
-    if (error.cause.code === 'ENOTFOUND') {
-        return `Link não encontrado!.`
-    }else{
-        return `Não foi possivel identificar o erro!.`
-    }
-}
-async function statusLinks(arrayLinks) {
+async function verificarStatus(arraLinks){
     return await Promise.all(
-        arrayLinks.map(async (url) => {
+        arraLinks.map(async(url)=>{
             try {
                 const response = await fetch(url)
-                return response.status
+                return response.status                
             } catch (error) {
-                return remanejarErro(error)
-            }
+                if(error.cause.code === 'ENOTFOUND'){
+                    return `Link não encontrado!.`
+                }else{
+                    return `Não foi possivel identificar o erro!.`
+                }
+            }                    
         })
     )
 }
 
 export default async function listaValidada(lista) {
-    const arrayLinks = extrairLinks(lista)
-    const status = await statusLinks(arrayLinks)  
+    const arraLinks = extrairLinks(lista)
+    const status = await verificarStatus(arraLinks)
     return lista.map((obj, indice) => ({
         ...obj,
-        Status: status[indice]
+        Status:status[indice]
     }))
 }
