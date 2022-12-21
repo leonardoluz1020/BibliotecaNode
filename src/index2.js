f592f68b336dc3e0c48e1ff77df407107ff345e3
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-21 12:01:35 -0300

    Criando arquivo index2

--- src/index2.js
+++ src/index2.js
@@ -1,25 +1,24 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-function tratarErro(error){
-    throw new Error(chalk.red(error.code, 'Não há arquivo na pasta!.'))    
+function remanejarErro(error){
+    throw new Error(chalk.red(error.code, 'Não há arquivo na pasta'))
 }
 
 function extrairLinks(texto){
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
     const capturas = [...texto.matchAll(regex)]
     const resultados = capturas.map((captura) => ({[captura[1]]: captura[2]}))
-    return resultados.length !== 0 ? resultados : 'Não há links no arquivo'
+    return resultados.length !== 0 ? resultados : 'Não há link no arquivo!.'
 }
-
-
+ 
 async function pegarArquivo(caminho){
-    const encoding = 'utf-8'
+    const encoding = 'utf-8';
     try {
-        const texto = await fs.promises.readFile(caminho,encoding)
-        return extrairLinks(texto)                
+        const texto = await fs.promises.readFile(caminho, encoding);
+        return extrairLinks(texto)
     } catch (error) {
-        tratarErro(error)        
+        remanejarErro(error)
     }
 }
 
