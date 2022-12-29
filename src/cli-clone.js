a12b97f16de53c796a80c9ddaec1107fcc1f36aa
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-29 19:24:01 -0300

    Criando index02.js e Cli-clone.js

--- src/cli-clone.js
+++ src/cli-clone.js
@@ -1,47 +1,49 @@
-import pegarArquivo from "./index2.js";
+/*  Imports das Lib e FrameWorks */
+import validarLinks from './validacao-http.js'
+import chalk from 'chalk';
 import fs from 'fs';
-import chalk from "chalk";
-import validarLinks from "./validacao-http.js";
+import pegarArquivo from './index2.js';
+const caminho = process.argv;
 
-const caminho = process.argv
-
-async function imprmirLista(valida, listas, nomeArquivo = ''){
+async function imprimirLista(valida, listas, nomeArquivo = '') {
     if (valida) {
         console.log(
-            chalk.yellow('Lista de links'),
-            chalk.bgBlue(nomeArquivo),
-          await  validarLinks(listas)
-        );
+            chalk.yellow('Lista de Links'),
+            chalk.bgGreen(nomeArquivo),
+           await validarLinks(listas)
+        )
     } else {
         console.log(
-            chalk.yellow('Lista de links'),
-            chalk.bgBlue(nomeArquivo),
+            chalk.yellow('Lista de Links'),
+            chalk.bgGreen(nomeArquivo),
             listas
-        );
+        )
+
     }
 }
 
-
-async function processarTexto(argumento) {
-    const caminho = argumento[2];
-    const valida = argumento[3] === '--valida';
+async function processarArquivo(argumentos) {
+    const caminho = argumentos[2];
+    const valida = argumentos[3] === '--valida';
     try {
         fs.lstatSync(caminho)
     } catch (error) {
-        if (error.code === 'ENOENT') {
-            console.log(chalk.red('Caminho de pasta ou arquivo incorreto!.'))
+        if(error.code === 'ENOENT'){
+            console.log(chalk.red(`Caminho de pasta ou arquivo incorreto!.`))
         }
         return
     }
+    
     if (fs.lstatSync(caminho).isFile()) {
         const lista = await pegarArquivo(caminho);
-        imprmirLista(valida, lista);
+        imprimirLista(valida, lista);
     } else if (fs.lstatSync(caminho).isDirectory()) {
-        const resultados = await fs.promises.readdir(caminho);
-        resultados.forEach(async (nomeArquivo) => {
+        const arquivos = await fs.promises.readdir(caminho);
+        arquivos.forEach(async (nomeArquivo) => {
             const listas = await pegarArquivo(`${caminho}/${nomeArquivo}`);
-            imprmirLista(valida, listas, nomeArquivo);
+            imprimirLista(valida, listas, nomeArquivo)
         })
     }
 }
-processarTexto(caminho);
+
+processarArquivo(caminho);
--- src/index2.js
+++ src/index2.js
@@ -1,28 +1,28 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-function remanejarErro(error) {
-    throw new Error(chalk.red(error.code, 'Não há aquivo no diretorio'));
-}
-function extrairLinks(texto){
-    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const capturas = [...texto.matchAll(regex)];
-    const resultados = capturas.map((captura) => ({[captura[1]]: captura[2]}));
-    return resultados.length !== 0 ? resultados : 'Não há link no arquivo'; 
+function tratarErro(error) {
+    throw new Error(chalk.red(error.code, 'Não há arquivo no diretorio!.'))
 }
 
+function extrairLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
+    const capturas = [...texto.matchAll(regex)]
+    const resultados = capturas.map((captura) => ({[captura[1]]:captura[2]}))
+    return resultados.length !== 0 ? resultados: `Não há link no arquivo!.`
+}
 
 async function pegarArquivo(caminho) {
     const encoding = 'utf-8';
     try {
         const texto = await fs.promises.readFile(caminho, encoding);
-        return extrairLinks(texto)
+        return extrairLinks(texto);
     } catch (error) {
-        remanejarErro(error)
+        return tratarErro(error);
     }
+
 }
 
 export default pegarArquivo;
-
 // expressão regular
 //  /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm
