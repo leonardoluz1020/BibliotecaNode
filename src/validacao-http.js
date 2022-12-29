f32fb4271ad9818fa2a887db3c926be710830f0e
Leonardo Luz <leonardoluz10@hotmail.com>
2022-12-29 19:38:40 -0300

    Criando arquivo validação-http.js

--- src/validacao-http.js
+++ src/validacao-http.js
@@ -1,30 +1,33 @@
 function extrairLinks(listas) {
-    return listas.map((objLinks) => Object.values(objLinks).join());
+    return listas.map((obj) => Object.values(obj).join());
 }
 
-async function statusLinks(arrayLinks) {
+async function verificarLinks(arrayLinks) {
     return await Promise.all(
         arrayLinks.map(async (url) => {
             try {
-                const response = await fetch(url)
-                return response.status
+                const res = await fetch(url);
+                return res.status;
             } catch (error) {
                 if (error.cause.code === 'ENOTFOUND') {
                     return `Link não encontrado!.`
-                }else {
+                } else {
                     return `Não foi possivel identificar o erro!.`
                 }
             }
         })
+
     )
+
 }
 
 async function validarLinks(listas) {
-    const arrayLinks = extrairLinks(listas);
-    const status = await statusLinks(arrayLinks);
+    const arrayLinks = extrairLinks(listas)
+    const texto = await verificarLinks(arrayLinks)
     return listas.map((obj, indice) => ({
         ...obj,
-        Status: status[indice]
+        Status: texto[indice]
     }))
 }
+
 export default validarLinks;
